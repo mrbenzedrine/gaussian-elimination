f0f540c5c24900b57945640d126725a8d8fe04eb
Yousef Moazzam <mrbenzedrine@live.co.uk>
2018-01-19 04:41:26 +0000

    Implement partial pivoting

--- main.c
+++ main.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
+#include <math.h>
 
 #define COLUMNS 3 // no of columns of the matrix
-#define ROWS 3 // no of rows of the matrix
+#define ROWS 2 // no of rows of the matrix
 
 void print_matrix(double *row_ptrs[]);
 void swap_rows(double *row_ptrs[], int row_a_index, int row_b_index);
 void subtract_rows(double *row_ptrs[], int row_a_index, int row_b_index, double multiplier, int start_column);
+int perform_partial_pivoting(double *row_ptrs[], double abs_pivot_value, int pivot_row_index, int pivot_column_index);
 
 main(){
 
     // work with a hard coded matrix for now
     double matrix_values[COLUMNS * ROWS] = {
-        1,2,4,3,8,14,2,6,13
+        0.00300, 59.14, 59.17, 5.291, -6.130, 46.78
     };
     // array of pointers that point to the start of a matrix row
     double *row_ptrs[ROWS]; 
     
     row_ptrs[0] = &matrix_values[0];
     row_ptrs[1] = &matrix_values[3];
-    row_ptrs[2] = &matrix_values[6];
 
     // start elimination
 
@@ -27,6 +28,15 @@ main(){
     double *pivot_row_ptr, *eliminate_row_ptr;
 
     for(k = 0; k < ROWS - 1; k++){
+        if( fabs(*(row_ptrs[k] + k)) < 1 ){
+            // perform partial pivoting
+            int row_index_of_largest_pivot;
+            row_index_of_largest_pivot = perform_partial_pivoting(row_ptrs, fabs(*(row_ptrs[k] + k)), k, k);
+            if(row_index_of_largest_pivot > 0){
+                // a row with a larger absolute value pivot was found, so swap them
+                swap_rows(row_ptrs, k, row_index_of_largest_pivot);
+            }
+        }
         pivot_row_ptr = row_ptrs[k];
         pivot = *(pivot_row_ptr + k);
         for(i = k + 1; i < ROWS; i++){
@@ -80,5 +90,27 @@ void subtract_rows(double *row_ptrs[], int row_a_index, int row_b_index, double 
     for(i = 0; i < COLUMNS; i++, temp_row_a_ptr++, temp_row_b_ptr++){
         *temp_row_a_ptr -= multiplier * *temp_row_b_ptr;
     }
+
+}
+
+// returns the index of the row with the pivot with the largest absolute value
+int perform_partial_pivoting(double *row_ptrs[], double abs_pivot_value, int pivot_row_index, int pivot_column_index){
+
+    int i; // loop variable
+    int row_index_of_largest_pivot = -1; // row index with largest pivot absolute value
+    double *temp_row_ptr;
+
+    // offset row_ptrs to start at the row below the original pivot row
+    row_ptrs += pivot_row_index + 1;
+
+    for(i = pivot_row_index + 1; i < ROWS; i++, row_ptrs++){
+        temp_row_ptr = *row_ptrs;
+        if( fabs(*(temp_row_ptr + pivot_column_index)) > abs_pivot_value ){
+            abs_pivot_value = fabs(*(temp_row_ptr + pivot_column_index));
+            row_index_of_largest_pivot = i;
+        }
+    }
+
+    return row_index_of_largest_pivot;
 
 }
